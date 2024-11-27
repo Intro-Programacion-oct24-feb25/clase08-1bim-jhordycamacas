a697ca368b11833fae773e0f4f8969c81377dbaf
jhordycamacas <jhordycamacas9@gmail.com>
2024-11-27 11:47:40 -0500

    cambios5

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
@@ -7,13 +7,15 @@
  sean ingresados por teclado por el usuario.
  */
 package ejemplos02;
-/**
- *
- * @author reroes
- */
+
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Ejemplo05 {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         // 2. numero, i[0, n]
         int numero;
         // 3. potencia, i[0, n]
@@ -21,19 +23,21 @@ public class Ejemplo05 {
         // 6. resultado, i[0, n]
         int resultado;
         // numero <-- 4
-        numero = 4;
-        // 8. potencia <-- 3
-        potencia = 3;
+        System.out.println("Ingrese el numero a elevar");
+        numero = entrada.nextInt();
+        System.out.println("Ingrese la potencia");
+        potencia = entrada.nextInt();
         // 9. resultado <-- 1
         resultado = 1;
 
         int contador = 1;
 
         do {
+
             resultado = resultado * numero;
             contador = contador + 1;
         } while (contador <= potencia);
 
-        System.out.printf("%d\n", resultado);
+        System.out.printf("El resultado es: %d\n", resultado);
     }
 }
